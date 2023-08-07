1ef573339db900b6ae96cb34eb0a5cecee0e153e
lukinovec <lukinovec@gmail.com>
2023-08-07 12:23:42 +0200

    Initial setup

---
+++ routes/tenant.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Product;
+use Illuminate\Support\Facades\Route;
+use Stancl\Tenancy\Middleware\InitializeTenancyBySubdomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromUnwantedDomains;
+
+/*
+|--------------------------------------------------------------------------
+| Tenant Routes
+|--------------------------------------------------------------------------
+|
+| Here you can register the tenant routes for your application.
+| These routes are loaded by the TenantRouteServiceProvider.
+|
+| Feel free to customize them however you want. Good luck!
+|
+*/
+
+Route::middleware([
+    'web',
+    InitializeTenancyBySubdomain::class,
+    PreventAccessFromUnwantedDomains::class,
+])->group(function () {
+    Route::get('/', function () {
+        return Product::all();
+    });
+});
